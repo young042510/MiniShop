99675566198936e462a106645e36ac2ff7feb5c8
jaeyoung <young042510@gmail.com>
2025-01-08 17:16:12 +0900

    배송도메인

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @JsonIgnore
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; //READY, COMP
+}
