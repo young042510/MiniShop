10428b103fa0817771d9a1ddf914a46ea551ef06
jaeyoung <young042510@gmail.com>
2025-01-09 12:13:59 +0900

    회원테스트

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,34 +23,25 @@ public class MemberServiceTest {
 
     @Test
     public void 회원가입() throws Exception {
-
         //Given
         Member member = new Member();
         member.setName("kim");
-
-        //when
+        //When
         Long saveId = memberService.join(member);
-
         //Then
         assertEquals(member, memberRepository.findOne(saveId));
     }
-
     @Test(expected = IllegalStateException.class)
     public void 중복_회원_예외() throws Exception {
         //Given
         Member member1 = new Member();
         member1.setName("kim");
-
         Member member2 = new Member();
         member2.setName("kim");
-
-        //When
         //When
         memberService.join(member1);
-        memberService.join(member2); //예외가 발생해햐 한다
-
+        memberService.join(member2); //예외가 발생해야 한다.
         //Then
-        fail("에외가 발생해야 한다");
-
+        fail("예외가 발생해야 한다.");
     }
 }
