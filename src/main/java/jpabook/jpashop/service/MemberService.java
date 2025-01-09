01548cfe7fa0bb6e7e52fae5250b75a08caec036
jaeyoung <young042510@gmail.com>
2025-01-09 12:13:54 +0900

    회원서비스

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -13,38 +13,29 @@ import java.util.List;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberService {
-
-    @Autowired
-    MemberRepository memberRepository;
-
-    /*
-    * 회원가입
-    * */
-    @Transactional // 변경
+    private final MemberRepository memberRepository;
+    /**
+     * 회원가입
+     */
+    @Transactional //변경
     public Long join(Member member) {
-
         validateDuplicateMember(member); //중복 회원 검증
         memberRepository.save(member);
         return member.getId();
     }
-
     private void validateDuplicateMember(Member member) {
         List<Member> findMembers = memberRepository.findByName(member.getName());
         if (!findMembers.isEmpty()) {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         }
     }
-
-    /*
-    *  전체 회원 조회
-    * */
+    /**
+     * 전체 회원 조회
+     */
     public List<Member> findMembers() {
         return memberRepository.findAll();
     }
-
     public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
     }
-
-
 }
