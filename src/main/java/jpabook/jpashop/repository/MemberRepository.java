eff9b14625cf77cc53507798a34c6c82a603ba67
jaeyoung <young042510@gmail.com>
2025-01-09 12:13:49 +0900

    회원레포지토리

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -9,26 +9,20 @@ import java.util.List;
 
 @Repository
 public class MemberRepository {
-
     @PersistenceContext
     private EntityManager em;
-
-    public Long save(Member member) {
+    public void save(Member member) {
         em.persist(member);
-        return member.getId();
     }
-
     public Member findOne(Long id) {
         return em.find(Member.class, id);
     }
-
     public List<Member> findAll() {
         return em.createQuery("select m from Member m", Member.class)
                 .getResultList();
     }
-
     public List<Member> findByName(String name) {
-        return em.createQuery("select m from Member m where m.name = : name",
+        return em.createQuery("select m from Member m where m.name = :name",
                         Member.class)
                 .setParameter("name", name)
                 .getResultList();
